22760e2407d48c97443d87165097fa4784e73dbf
ttr <ttr@cs.odu.edu>
2020-06-04 21:48:31 -0400

    Updates

---
+++ precache-manifest.ede39a8fade93c1b39d8e31cf7308b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7523144ecab0b3ac6232efd5bcb2b6c3",
+    "url": "/mandarin-square/index.html"
+  },
+  {
+    "revision": "db5c51d1aea785c154ed",
+    "url": "/mandarin-square/static/css/main.832d8ca1.chunk.css"
+  },
+  {
+    "revision": "399242dd0f573a2bd328",
+    "url": "/mandarin-square/static/js/2.af58107c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mandarin-square/static/js/2.af58107c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5c51d1aea785c154ed",
+    "url": "/mandarin-square/static/js/main.c1ca2b53.chunk.js"
+  },
+  {
+    "revision": "4dd217769a293b4efe1b",
+    "url": "/mandarin-square/static/js/runtime-main.b0b87709.js"
+  }
+]);
